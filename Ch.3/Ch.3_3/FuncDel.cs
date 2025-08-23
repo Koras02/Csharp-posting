746564d2b5c4d61742a41948c4b16da36d6e493b
Koras02 <wjdgh0727@gmail.com>
2025-08-24 02:54:35 +0900

    C# 람다식 Func/Action

---
+++ Ch.3/Ch.3_3/FuncDel.cs
@@ -0,0 +1,11 @@
+﻿class FuncDel
+{
+    static void Main()
+    {
+        Func<int, int, int> add = (a, b) => a + b;
+        Console.WriteLine(add);
+
+        Func<string, int> strLength = s => s.Length;
+        Console.WriteLine(strLength("Hello, World!")); // 13
+    }
+}
---
+++ Ch.3/Ch.3_3/Lamda.cs
@@ -0,0 +1,17 @@
+﻿class Lamda
+{
+    static void Main()
+    {
+        // Normal Method
+        int Square(int x)
+        {
+            return x * x;
+        }
+
+            // Lamda Method
+            Func<int, int> square = x => x * x;
+
+        Console.WriteLine(square(5));
+    }
+
+}
